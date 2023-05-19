f7c7b948a0f0708c0134e3a7a858aa482d9aa4e7
Ruba_Bakhour <rabdullahbakhour@stu.kau.edu.sa>
2023-05-19 21:28:10 +0300

    Adding a adoptin method, still in progress. Adding class Shelter with notes

---
+++ src/Shelter.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ruba
+ */
+public class Shelter extends User{
+    
+    
+// A method to get avaialbe time
+// A method to change state of appointment
+}
---
+++ src/User.java
@@ -0,0 +1,40 @@
+
+public class User {
+
+    // a method to request adopting a cat from their owner while checking if the requirement are met
+//    public static void adoptionReq(Cat cat, Individual user, User owner) {
+//        // check if the conditions are met
+//        if (user.getHasCats() == cat.getLikesCats && user.getHasChildren() == cat.getLikesChildren) {
+//
+//            if( owner instanceof Individual) {  //if the the user is adopting from an individual
+//
+//                System.out.println("These are the owner contatct info, please contact them to arrage adoption.\n" + owner.getcontactInfo);
+//                cat.setAdoptionState("Pending"); // set cat's adoption state
+//
+//            } else { //if the the user is adopting from a shelter
+//
+//                System.out.println("please pick a time for meeting: ");
+//                System.out.println(owner.getAvailableTimes);
+//                // scanner for user input
+//                String timeChoice = input.next();
+//                owner.getAvailableTimes.appoint(timeChoice);
+//                System.out.println("You have succesfully made an apponitment with the shelter!");
+//                cat.setAdoptionState("Pending"); // set cat's adoption state
+//
+//            }
+//
+//        } else {
+//            System.out.println("Sorry! you cannot adopt this cat due to unmet conditions");
+//        }
+//
+//    }
+//// a method for approving adoption reqest 
+//    public static void adoptionApprove(Cat cat) {
+//        cat.setAdoptionState("Adopted");  // set cat's adoption state
+//    }
+//// a method for canceling adoption reqest
+//    public static void adoptionCancel(Cat cat) {
+//        cat.setAdoptionState("Available");  // set cat's adoption state
+//    }
+
+}
