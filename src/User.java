4c4eb58f5f7b399451dedc100eb58b80ac627a83
Raghad_Harith <raharith@stu.kau.edu.sa>
2023-05-21 01:20:33 +0300

    Added OfferCat method

--- src/User.java
+++ src/User.java
@@ -36,5 +36,37 @@ public class User {
 //    public static void adoptionCancel(Cat cat) {
 //        cat.setAdoptionState("Available");  // set cat's adoption state
 //    }
-
+    
+    
+    ////// 
+    /* Offer Cat Method !!
+    public void OfferCat(){
+// Prompt the user to enter cat info :
+        System.out.println("Enter cat information :(Note : You should enter it with this order : catId, catName, sex, age, breed, bound (Enter the name please), behavior, disabled, medicalHistory, likesCats, likesChildrens, adoptionState, adoptionReason, adoptionFees");
+      // Fill in cat info :
+      
+         int catId = input.nextInt();
+         String catName = input.next();
+         String sex = input.next();
+         int age = input.nextInt();
+         String breed = input.next();
+         String catName = input.next();
+         Cat bound; Searchbound(String catName);
+         String behavior = input.next();
+         boolean disabled = input.nextBoolean();
+         String medicalHistory = input.next();
+         boolean likesCats = input.nextBoolean();
+         boolean likesChildrens = input.nextBoolean();
+         String adoptionState = input.next();
+         String adoptionReason = input.next();
+         int adoptionFees = input.nextInt();
+      
+     // Create Cat object : 
+      Cat newCat = new Cat(catId, catName, sex, age, breed, bound, behavior, disabled, medicalHistory, likesCats, likesChildrens, adoptionState, adoptionReason,adoptionFees);
+      
+      // Add it to the cats database ( Arraylist ) :
+        cat.addNewCattoDatabase(cat) ;
+     
+        
+    } Offer cat end here !! */
 }
