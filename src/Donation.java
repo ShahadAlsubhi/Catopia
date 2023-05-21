bc26c7732201818a558f26e2baa0b4dfbd9d93fb
Sarah Albearoti <sezzatalbearoti@stu.kau.edu.sa>
2023-05-21 16:56:55 +0300

    Merge origin/master
    Conflicts:
    	src/Shelter.java
    	src/User.java

---
+++ src/Donation.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class Donation {
+    private Donation DonationType;
+    private Date DonationDate;
+    private String UserID;
+    private String ShelterID;  
+}
