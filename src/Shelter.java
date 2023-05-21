f9eb8b37803d30b1e044e264e08e4d640421c69f
Sarah Albearoti <sezzatalbearoti@stu.kau.edu.sa>
2023-05-21 15:30:39 +0300

    Merge origin/master
    Conflicts:
    	src/Shelter.java
    	src/User.java

--- src/Shelter.java
+++ src/Shelter.java
@@ -7,8 +7,14 @@
  *
  * @author Ruba
  */
+import java.util.*;
+
 public class Shelter extends User{
-    
+    private String ShelterName;
+    private boolean Verification;
+    private ArrayList<String> RecievedDonation;
+    private ArrayList<String> ReservedAppointment;
+  
     
 // A method to get avaialbe time
 // A method to change state of appointment
